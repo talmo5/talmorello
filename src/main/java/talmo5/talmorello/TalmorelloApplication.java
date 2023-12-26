f8fdf8c83b876b4eb3375fc2a3453c37cf45b280
mistarson <yeowuli2@naver.com>
2023-12-26 10:23:47 +0900

    Initial commit

---
+++ src/main/java/talmo5/talmorello/TalmorelloApplication.java
@@ -0,0 +1,13 @@
+package talmo5.talmorello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalmorelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalmorelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/talmo5/talmorello/TalmorelloApplicationTests.java
@@ -0,0 +1,13 @@
+package talmo5.talmorello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalmorelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
