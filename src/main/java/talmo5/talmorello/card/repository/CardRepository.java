0372f74604b664c62d0537da6700991412930420
Dongha <wkdehdgk159@gmail.com>
2023-12-27 14:32:24 +0900

    Refactor: 카드 순서의 최대값 조회 시 카드 엔티티가 아닌 column 값만 받아오도록 수정

--- src/main/java/talmo5/talmorello/card/repository/CardRepository.java
+++ src/main/java/talmo5/talmorello/card/repository/CardRepository.java
@@ -1,10 +1,11 @@
 package talmo5.talmorello.card.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import talmo5.talmorello.card.entity.Card;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
 
-    Optional<Card> findTopByOrderByOrdersDesc();
+    @Query("SELECT c.orders FROM Card c ORDER BY c.orders DESC LIMIT 1")
+    Integer getLastOrders();
 }
--- src/main/java/talmo5/talmorello/card/service/CardService.java
+++ src/main/java/talmo5/talmorello/card/service/CardService.java
@@ -1,6 +1,5 @@
 package talmo5.talmorello.card.service;
 
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import talmo5.talmorello.card.dto.CreateCardDTO;
@@ -27,22 +26,12 @@ public class CardService {
         //userService 에서 userId에 해당하는 유저 받아오기
         //User user = userService.findUserById(userId);
 
-        int orders = findLastOrdersOfCard();
+        int orders = cardRepository.getLastOrders();
 
         Card card = createCardDTO.toEntity(createCardDTO.cardTitle(), user, orders + 1);
 
         cardRepository.save(card);
 
-        //추후에 유저에서 직접 유저네임 받아올 것.
         return CreateCardDTO.Response.of(card);
-    }
-
-    private int findLastOrdersOfCard() {
-
-        Optional<Card> lastOrdersCard = cardRepository.findTopByOrderByOrdersDesc();
-
-        if(lastOrdersCard.isPresent()) return lastOrdersCard.get().getOrders();
-
-        return 0;
     }
 }
