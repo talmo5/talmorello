b14ff66332c32bbd667ab0a2cfd3813887cf68d9
mistarson <yeowuli2@naver.com>
2023-12-27 21:06:38 +0900

    Rename: User 테이블명 Users로 변경
    H2 DB에서는 User가 예약어기 때문에 테스트를 위해 변경

--- src/main/java/talmo5/talmorello/user/entity/User.java
+++ src/main/java/talmo5/talmorello/user/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +18,7 @@ import talmo5.talmorello.user.constant.SocialType;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User extends BaseTime {
 
     @Id
