93558e7560e5b4d7d5110837903a08cc41673610
rinyejo <rinyejoin@gmail.com>
2023-12-27 17:52:24 +0900

    normal user signup 기능 수정

--- src/main/java/talmo5/talmorello/user/service/UserService.java
+++ src/main/java/talmo5/talmorello/user/service/UserService.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import talmo5.talmorello.global.exception.user.UserAlreadyExistException;
+import talmo5.talmorello.user.dto.LoginRequestDto;
 import talmo5.talmorello.user.dto.SignupRequestDto;
 import talmo5.talmorello.user.entity.User;
 import talmo5.talmorello.user.repository.UserRepository;
@@ -18,16 +18,9 @@ public class UserService {
 
     public void signup(SignupRequestDto requestDto){
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
+        String password = passwordEncoder.encode(requestDto.getPassword());
         String email = requestDto.getEmail();
 
-//        // 회원 중복 확인
-//        try {
-//            User user = userRepository.findByUsername(username)
-//                    .orElseThrow(() -> new UserAlreadyExistException("이미 존재하는 이름입니다."));
-//        } catch (UserAlreadyExistException e) {
-//
-//        }
 
         Optional<User> checkUsername = userRepository.findByUsername(username);
         if (checkUsername.isPresent()) {
@@ -39,15 +32,11 @@ public class UserService {
             throw new IllegalArgumentException("이미 사용중인 email입니다.");
         }
 
-//        try {
-//            User user = userRepository.findByEmail(email)
-//                    .orElseThrow(() -> new IllegalArgumentException("중복된 Email 입니다."));
-//        } catch (IllegalArgumentException e) {
-//
-//        }
 
         User user = new User(username, password, email);
         userRepository.save(user);
     }
+
+
 
 }
