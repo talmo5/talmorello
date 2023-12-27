d08bea5698a89cf253d4421fb57d096ce617eddd
mistarson <yeowuli2@naver.com>
2023-12-28 01:21:36 +0900

    Feature: 댓글 삭제 API 기능 구현

--- src/main/java/talmo5/talmorello/comment/respository/CommentRepository.java
+++ src/main/java/talmo5/talmorello/comment/respository/CommentRepository.java
@@ -2,7 +2,8 @@ package talmo5.talmorello.comment.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import talmo5.talmorello.comment.entity.Comment;
+import talmo5.talmorello.comment.respository.custom.CustomCommentRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CustomCommentRepository {
 
 }
