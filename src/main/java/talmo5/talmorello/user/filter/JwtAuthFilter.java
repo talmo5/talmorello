04b288f7a8e5d0dae2c03a83b078a6c70bd62c94
mistarson <yeowuli2@naver.com>
2023-12-29 17:15:45 +0900

    Fix: 컴파일되지 않은 코드 삭제

--- src/main/java/talmo5/talmorello/user/filter/JwtAuthFilter.java
+++ src/main/java/talmo5/talmorello/user/filter/JwtAuthFilter.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,39 +16,31 @@ import talmo5.talmorello.user.jwt.JwtUtil;
 @Slf4j(topic = "JwtAuthFilter")
 @Component
 @RequiredArgsConstructor
-@Order(1)
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res,
+            FilterChain filterChain)
             throws ServletException, IOException {
 
-            String tokenValue = jwtUtil.getTokenFromRequest(req);
+        String tokenValue = jwtUtil.getTokenFromRequest(req);
 
         if (StringUtils.hasText(tokenValue)) {
             throw new IllegalArgumentException("Not Found Token");
         }
 
-                String token = jwtUtil.substringToken(tokenValue);
+        String token = jwtUtil.substringToken(tokenValue);
 
-                if (!jwtUtil.validateToken(token)) {
-                    throw new IllegalArgumentException("Token Error");
-                }
-
-                String username = jwtUtil.getUsernameFromToken(token);
-
-                User user = userRepository.findByUsername(username).orElseThrow(() ->
-                        new NullPointerException("Not Found User")
-                );
-
-                req.setAttribute("user", user);
-                filterChain.doFilter(req, res);
-            } else {
-                throw new IllegalArgumentException("Not Found Token");
-            }
+        if (!jwtUtil.validateToken(token)) {
+            throw new IllegalArgumentException("Token Error");
         }
+
+        Long userId = jwtUtil.getUserIdFromToken(token);
+
+        req.setAttribute("userId", userId);
+        filterChain.doFilter(req, res);
     }
 
     @Override
