5365413e669dd92c7d7db97bd082307e65276383
mistarson <yeowuli2@naver.com>
2023-12-30 17:36:20 +0900

    Feature: 로그인한 userId를 바인딩해주는 argumentResolver 구현

---
+++ src/main/java/talmo5/talmorello/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package talmo5.talmorello.global.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import talmo5.talmorello.global.argumentresolver.UserIdArgumentResolver;
+import talmo5.talmorello.user.jwt.JwtUtil;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserIdArgumentResolver(jwtUtil));
+    }
+}
